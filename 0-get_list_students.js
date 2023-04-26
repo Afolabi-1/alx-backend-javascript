c85e62fbe47fad7b091929802ffc04f667219904
afolabi <72478870+Afolabi-1@users.noreply.github.com>
2023-04-26 21:56:54 +0100

    Create 0-get_list_students.js
    this is the answer to the first task

---
+++ 0-get_list_students.js
@@ -0,0 +1,10 @@
+functon getListStudents() {
+  //Defines an List of Students with Id, FirstName And Location Attributes
+  const students = [
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
+  ];
+  // returns the list of students
+  return students;
+}
